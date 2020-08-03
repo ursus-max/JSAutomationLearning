24140720eb3c698059bf36168c41a0b4dc111471
ursus-max <soft.mnes@gmail.com>
2020-08-03 19:12:20 +0300

    Operators_tasks
    Operators

---
+++ operators.js
@@ -0,0 +1,30 @@
+/*
+let a = 1, b = 1
+
+let c = ++a // 2
+let d = b++ // 1
+*/
+//let a = 2
+//let x = 1 + (a *= 2)
+//a = 4
+// x = 5
+
+""+1+0 // 10
+""- 1 + 0// 1
+6 / "3" //2 
+"2" * "3" //6
+4+5+"px" // 9px
+"$" + 4 + 5 //$45
+"4" - 2 // 2
+"4px" - 2 //NaN
+7 / 0 //Infinity
+"  -9  " + 5 //  -9  5
+"  -9  " - 5 //  -14
+null + 1 //null
+undefined + 1 // undefined 
+" \t \n" - 2//-2
+
+let a = +prompt ("First number", 1);
+let b = +prompt ("Second number" , 2);
+
+alert (a+b)
