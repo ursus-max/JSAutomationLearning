97ff09bcbff84e8cdacaa69c41f07daf64bff932
ursus-max <soft.mnes@gmail.com>
2020-08-06 16:49:47 +0300

    function_tasks_2

--- Functions/function_basic.js
+++ Functions/function_basic.js
@@ -4,3 +4,13 @@
 function checkAge(age){
     return (age > 18 ?  true :  confirm ('Did parents allow you?')) 
 }
+
+    function min(a,b){
+        return (a>b) ? b : a
+    }
+
+function pow (x,n){
+    for (let i=1; i<=n; i++){
+        return x = x*x
+    }
+}
