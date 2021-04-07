e71292f241d6544495a1bf12a47f139ba02209a5
ursus-max <soft.mnes@gmail.com>
2021-04-07 14:56:50 +0300

    Objects

---
+++ Objects/object.js
@@ -0,0 +1,39 @@
+let user = {
+    "name": "John",
+    "surname": "Smith",
+}
+
+user.name="Pete"
+delete user.name
+
+//
+
+function isEmpty(obj){
+    for (let key in obj){
+        return false
+    }
+    return true
+}
+
+//
+
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+
+for (let key in salaries){
+    sum+=salaries[key]
+}
+
+//
+
+function multiplyNumeric(obj){
+    for (let key in obj){
+        if (typeof(obj[key])=="number"){
+            obj[key]=obj[key]*2
+        }
+    }
+}
