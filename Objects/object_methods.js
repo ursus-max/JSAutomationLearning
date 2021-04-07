0d612746323e9f099c8ff02522a49b8565a4b2e4
ursus-max <soft.mnes@gmail.com>
2021-04-07 17:48:33 +0300

    Objects_methods

---
+++ Objects/object_methods.js
@@ -0,0 +1,27 @@
+// let user = {
+//     name:"John",
+//     go:function(){alert(this.name)}
+// }
+
+// (user.go)()
+
+//
+
+// let calculator = {
+//     read(){
+//         this.a=+prompt("Enter first value");
+//         this.b=+prompt("Enter second value");
+        
+//     },
+//     sum(){
+//         return this.a+this.b
+//     },
+//     mul(){
+//         return this.a*this.b
+//     }
+
+// };
+
+// calculator.read();
+// alert (calculator.sum());
+// alert (calculator.mul());
